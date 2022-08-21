634b3c73ec010cf68d2989d4b6b1f39946b86021
adropofilm <adropofilm@gmail.com>
2022-08-21 17:39:10 -0500

    convert swagger 2 to postman 2

--- lib/ex_api_spec_converter.ex
+++ lib/ex_api_spec_converter.ex
@@ -1,20 +1,156 @@
 defmodule ExApiSpecConverter do
   import Helpers
-  
+
   @moduledoc """
   Documentation for `ExApiSpecConverter`.
   """
 
-  @doc """
-  Hello world.
+  def convert(filename, doc_id) do
+    filename
+    |> load_specs
+    |> prepare_specs
+    |> fill_template(doc_id)
+  end
 
-  ## Examples
+  defp prepare_specs(specs) do
+    {paths, schema_defs} =
+      specs
+      |> extract_data
 
-      iex> ExApiSpecConverter.hello()
-      :world
+    schema_defs
+    |> convert_schemas
+    |> convert_requests(paths)
+    |> alphabetize_requests()
+    |> convert_folders
+  end
 
-  """
-  def hello do
-    :world
+  def convert_schemas(schemas) do
+    {objs, ref_objs, arrays} = order_schemas(schemas)
+
+    objs
+    |> prepare_schemas
+    |> prepare_schemas(ref_objs)
+    |> prepare_schemas(arrays)
+  end
+
+  def prepare_schemas(completed \\ %{}, schemas)
+  def prepare_schemas(completed, [{name, body}|t]) do
+    cond do
+      body["type"] == "object" ->
+        with {schema_name, body} <- build_response(name, body["properties"], completed) do
+          completed
+          |> Map.put(schema_name, body)
+          |> prepare_schemas(t)
+        end
+      body["type"] == "array" ->
+        if Map.has_key?(body["items"], "$ref") do
+          ref_name = schema_name(body["items"]["$ref"])
+
+          completed
+          |> Map.put(body["title"], [completed[ref_name]])
+          |> prepare_schemas(t)
+        else
+          completed
+          |> Map.put(body["title"], body["example"])
+          |> prepare_schemas(t)
+        end
+    end
+  end
+  def prepare_schemas(completed, []), do: completed
+
+  def build_response(name, properties, completed) do
+    prop_names = Map.keys(properties)
+
+    {body, _refs} = populate_properties(name, properties, prop_names, completed)
+
+    {name, body}
+  end
+
+  def order_schemas(schemas, objs \\ [], ref_objs \\ [], arrays \\ [])
+  def order_schemas([{_name, body} = h|t], objs, ref_objs, arrays) do
+    cond do
+      body["type"] == "object" and !has_ref?(body["properties"]) ->
+        order_schemas(t, [h|objs], ref_objs, arrays)
+      body["type"] == "object" and has_ref?(body["properties"]) ->
+        order_schemas(t, objs, [h|ref_objs], arrays)
+      body["type"] == "array" ->
+        order_schemas(t, objs, ref_objs, [h|arrays])
+    end
+  end
+  def order_schemas([], objs, ref_objs, arrays) do
+    {objs, ref_objs, arrays}
+  end
+
+  def populate_properties(name, schema, prop_names, completed, refs \\ %{}, body \\ %{})
+  def populate_properties(name, schema, [h|t], completed, refs, body) do
+    if Map.has_key?(schema[h], "$ref") do
+      schema_name = schema_name(schema[h]["$ref"])
+      if Map.has_key?(completed, schema_name) do
+        updated_body = Map.put(body, h, completed[schema_name])
+        populate_properties(h, schema, t, completed, refs, updated_body)
+      else
+        updated_refs = Map.put(refs, h, schema_name(schema[h]["$ref"]))
+        populate_properties(name, schema, t, completed, updated_refs, body)
+      end
+    else
+      updated_body = Map.put(body, h, schema[h]["example"])
+      populate_properties(name, schema, t, completed, refs, updated_body)
+    end
+  end
+  def populate_properties(_name, _schema, [], _completed, refs, body) do
+    {body, refs}
+  end
+
+  defp convert_requests(responses, requests, converted_requests \\ %{})
+  defp convert_requests(responses, [{path_str, raw_requests} | tail], converted_requests) do
+    updated_requests =
+      path_str
+      |> map_request(raw_requests, responses)
+      |> add_to_folder(get_folder_name(raw_requests), converted_requests)
+    convert_requests(responses, tail, updated_requests)
+  end
+  defp convert_requests(_responses, [], converted_requests), do: converted_requests
+
+  defp convert_folders(converted_requests) do
+    Enum.map converted_requests, fn {folder_name, requests} ->
+      create_folder(requests, folder_name)
+    end
+  end
+
+  defp map_request(path, request_map, responses) do
+    request_map
+    |> Stream.map(fn {method, request_body} ->
+      build_request(responses, path, method, request_body)
+    end)
+    |> Enum.to_list
+  end
+
+  defp get_folder_name(request_map) do
+    {_method, request} = Enum.random(request_map)
+    request["operationId"]
+  end
+
+  defp add_to_folder(request_list, folder_name, converted_requests) do
+    case Map.has_key?(converted_requests, folder_name) do
+      true ->
+        Map.put(converted_requests, folder_name, request_list ++ converted_requests[folder_name])
+      false ->
+        Map.put(converted_requests, folder_name, request_list)
+    end
+  end
+
+  def has_ref?(body) do
+    Enum.any?(body, fn {_property, body} -> Map.has_key?(body, "$ref") end)
+  end
+
+  def alphabetize_requests(converted_requests) do
+    converted_requests
+    |> Enum.reduce(%{}, fn {controller, routes}, acc ->
+      sorted_routes = Enum.sort_by(routes, &String.downcase(&1.name), &(&1 < &2))
+      Map.put(acc, controller, sorted_routes)
+    end)
+    |> Enum.sort(fn {first_key, _routes}, {second_key, _sec_routes} ->
+      first_key < second_key
+    end)
   end
 end
--- lib/helpers.ex
+++ lib/helpers.ex
@@ -1,15 +1,134 @@
 defmodule Helpers do
 
-  def fill_template(converted_requests, postman_id, collection_name, description) do
+  def get_paths(%{"paths" => paths}), do: paths |> Enum.into([])
+
+  def extract_data(%{"paths" => paths, "definitions" => defs}) do
+    {paths |> Enum.into([]), defs |> Enum.into([])}
+  end
+
+  def create_folder(requests, folder_name) do
+    %{
+      name: "#{folder_name}",
+      item: requests,
+      description: "Folder for #{folder_name}",
+      _postman_isSubFolder: true
+    }
+  end
+
+  def build_request(responses, path, method, %{"description" => descr, "summary" => name, "responses" => resp}) do
+    %{
+      name: name,
+      request: %{
+        method: String.upcase(method),
+        header: [],
+        body: %{
+          mode: "raw"
+        },
+        url: %{
+          raw: "https://api-stage.zego.io/api#{path}",
+          protocol: "https",
+          host: [
+            "api-stage",
+            "zego",
+            "io"
+          ],
+          path: create_path(path)
+        },
+        description: descr
+      },
+      response: generate_resp(Enum.into(resp, []), responses)
+    }
+  end
+
+  def create_response(body, status, code) do
+    %{
+      name: "#{code}-#{status}",
+      status: status,
+      code: code,
+      _postman_previewlanguage: "json",
+      header: [],
+      cookie: [],
+      body: body |> Poison.encode!
+    }
+  end
+
+  def fill_template(converted_requests, postman_id) do
     %{
       collection: %{
         info: %{
           _postman_id: postman_id,
-          name: collection_name,
-          description: description,
+          name: "Zego Core API - Docs",
+          description: "API empowering the Zego system",
           schema: "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
         },
         item: converted_requests
+      }
+    }
+  end
+
+  defp generate_resp(request_list, responses, response_list \\ [])
+  defp generate_resp([], _responses, response_list), do: response_list
+  defp generate_resp([{code, body}|tail], responses, response_list) do
+    code_int = code |> String.to_integer
+
+    if has_body?(body) do
+      if code_int in 300..599 do
+        %{"description" => descr} = body # TODO add schema key in future
+
+        response =
+          descr
+          |> get_msg
+          |> build_err_schema
+          |> create_response(descr, code_int)
+
+          generate_resp(tail, responses, [response | response_list])
+      else
+        %{"description" => descr, "schema" => %{"$ref" => ref}} = body
+
+        response =
+          create_response(responses[schema_name(ref)], descr, code_int)
+
+        generate_resp(tail, responses, [response | response_list])
+      end
+    else
+      response =
+        create_response(%{}, "No content", code_int)
+
+      generate_resp(tail, responses, [response | response_list])
+    end
+  end
+
+  def schema_name(schema) do
+    schema
+    |> String.split("#/definitions/")
+    |> Enum.at(1)
+  end
+
+  def has_body?(response) do
+    Map.has_key?(response, "schema")
+  end
+
+  def response_schema(%{"schema" => %{"$ref" => schema}}, responses) do
+    schema_name = schema_name(schema)
+    responses[schema_name]
+  end
+
+  defp create_path(path) do
+    [_slash|tail] = String.split(path, "/")
+    ["api"|tail]
+  end
+
+  # TODO remove after maps used for response descriptions
+  defp get_msg(msg) do
+    [_status|tail] = String.split(msg, " - ")
+    List.to_string(tail)
+  end
+
+  defp build_err_schema(msg) do
+    %{
+      success: false,
+      errors: %{
+        details: "#{msg}"
       }
     }
   end
